a87162dac13cd57eda4e851754e870dbf66e9bcc
vaishnavi0604 <112461449+vaishnavi0604@users.noreply.github.com>
2022-12-16 14:00:00 +0530

    Update start.py

--- start.py
+++ start.py
@@ -132,10 +132,10 @@ if option=='Staff':
                             break
                             
  else:
-    st.header("Admin")
-    choice=['View inventory','update product','delete product','Create user']
-    mode=st.selectbox(" ",choice)
-    if mode=='View inventory':
+      st.header("Admin")
+      choice=['View inventory','update product','delete product','Create user']
+       mode=st.selectbox(" ",choice)
+       if mode=='View inventory':
         table=usrc.read()
         st.table(table)
         count=usrc.count_drinks()
