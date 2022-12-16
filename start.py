96e07eb709aa40179b438f46e53d7201709cfb12
vaishnavi0604 <112461449+vaishnavi0604@users.noreply.github.com>
2022-12-16 14:03:27 +0530

    Update start.py

--- start.py
+++ start.py
@@ -10,7 +10,7 @@ import time
 usrc.create()
 @st.cache
 def model():
-    return torch.hub.load('ultralytics/yolov5', 'custom', path="best.pt",force_reload=True) 
+    return torch.hub.load('ultralytics/yolov5', 'custom', path="model.pt",force_reload=True) 
 
 model=model()
 class VideoProcessor:
